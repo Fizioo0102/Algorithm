6d82b35c9b134aef619399447de380441deda412
Fizioo0102 <107913312+Fizioo0102@users.noreply.github.com>
2022-08-20 22:07:41 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 216 ms, Memory: 18180 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        String s = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+s+"::fan:");
+        System.out.println(":fan::fan::fan:");
+
+    }
+}
