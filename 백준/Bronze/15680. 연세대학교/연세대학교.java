95dbbaeeaf1a28cfc13e730895f13217719ab932
Fizioo0102 <107913312+Fizioo0102@users.noreply.github.com>
2022-08-20 22:10:57 +0900

    [Bronze IV] Title: 연세대학교, Time: 204 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int n = scan.nextInt();
+
+        if(n==0) System.out.println("YONSEI");
+        else System.out.println("Leading the Way to the Future");
+
+    }
+}
